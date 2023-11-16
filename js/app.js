b8cb828cf445f28943c853d9118a85ff253c2c77
Vanina Federici <vanifederici@gmail.com>
2023-11-16 16:22:01 -0300

    fisrt commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const sectionPage = new fullpage("#fullpage", {
+  autoScrolling: true,
+  fitToSection: false,
+  fitToSectionDelay: 300,
+  easing: "easeInOutCubic",
+  scrollingSpeed: 700,
+  css3: true,
+  easingcss3: "ease-out",
+  loopBottom: false,
+  navigation: true,
+  menu: "#menu",
+  anchors: ["home", "about", "services", "work", "clients", "contact"],
+
+  showActiveTooltip: false,
+
+  verticalCentered: true,
+  afterLoad: function (origin, destination, direction, trigger) {
+    let changeLogo = document.getElementById("logo");
+    if (
+      destination.anchor === "home" ||
+      destination.anchor === "work" ||
+      destination.anchor === "services" ||
+      destination.anchor === "contact"
+    ) {
+      //pongo negro
+      changeLogo.setAttribute("src", "./media/logo-redandblack.png");
+    } else {
+      //pongo rojo
+      changeLogo.setAttribute("src", "./media/logo-white.png");
+    }
+  },
+});
